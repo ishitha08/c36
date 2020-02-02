66837e13f60b25ff7d565b13c85290367ea401bc
ilan M <ilanm@ilans-iMac.local>
2020-02-02 13:51:11 +0530

    explore classes

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,38 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountref = await database.ref("playerCount").once("value");
+      this.if(playerCountref.exists()){
+        playerCount = playerCountref.val();
+        player.getCount();
+
+      }
+      //player.getCount();
       form = new Form()
       form.display();
     }
   }
+   play(){
+     form.hide();
+     textSize(30);
+     text("gameStart",120,100);
+     Player.getPlayerInfo();
+     if(allPlayers!==undefined){
+       var display_position = 130;
+       if(plr==="player"+player.index)
+       fill("red");
+       else
+       fill("black");
+       displayPosition += 20;
+       text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,display_position);
+     }
+   }
+   if(keyIsDown(UP_ARROW)&& player.indev!==null){
+     player.distance+=50;
+     player.update();
+
+   }
 }
